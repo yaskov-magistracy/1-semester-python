3e1318a34393bc279e4fc84c776cd63008f6d7a1
Anton Yaskov <yaskovanton333@gmail.com>
2025-09-25 00:23:28 +0500

    Добавил .env

--- backend/config.py
+++ backend/config.py
@@ -1,41 +1,26 @@
 from pydantic_settings import BaseSettings
 import os
+from dotenv import load_dotenv
 
 class Settings(BaseSettings):
     def DATABASE_CONNECTION_STRING(self) -> str:
-        envVar = os.getenv('DATABASE_CONNECTION_STRING')
-        if envVar:
-            return envVar
-        return "postgresql+asyncpg://postgres:password@localhost:5432/python2"
+        return os.getenv('DATABASE_CONNECTION_STRING')
 
     def EMAIL_LOGIN(self) -> str:
-        envVar = os.getenv('EMAIL_LOGIN')
-        if envVar:
-            return envVar
-        return "chernorusy@mail.ru"
+        return os.getenv('EMAIL_LOGIN')
 
     def EMAIL_PASSWORD(self) -> str:
-        envVar = os.getenv('EMAIL_PASSWORD')
-        if envVar:
-            return envVar
-        return ""
+        return os.getenv('EMAIL_PASSWORD')
     
     def REDIS_HOST(self) -> str:
-        envVar = os.getenv('REDIS_HOST')
-        if envVar:
-            return envVar
-        return "127.0.0.1"
+        return os.getenv('REDIS_HOST')
     
     def REDIS_PORT(self) -> str:
-        envVar = os.getenv('REDIS_PORT')
-        if envVar:
-            return envVar
-        return "6379"
+        return os.getenv('REDIS_PORT')
     
     def REDIS_PASSWORD(self) -> str:
-        envVar = os.getenv('REDIS_PASSWORD')
-        if envVar:
-            return envVar
-        return "password"
+        return os.getenv('REDIS_PASSWORD')
 
+
+load_dotenv()
 settings = Settings()
