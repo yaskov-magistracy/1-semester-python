571a1a59900218d582dda87fb6419e4ab2a64dbb
Anton Yaskov <yaskovanton333@gmail.com>
2025-09-25 00:32:19 +0500

    Добавил текстовки в тесты

--- backend/tests/test_requests.py
+++ backend/tests/test_requests.py
@@ -2,8 +2,16 @@ import requests
 
 def test_logout_response():
     response = requests.post('http://127.0.0.1:8000/api/v1/accounts/logout')
-    assert response.status_code == 204
+    response2 = requests.post('http://127.0.0.1:8000/api/v1/accounts/logout')
+    assert response.status_code == 204, (
+        "Logout request should be idempotent"
+    )
+    assert response2.status_code == 204, (
+        "Logout request should be idempotent"
+    )
 
 def test_auth():
     response = requests.get('http://127.0.0.1:8000/api/v1/accounts/my')
-    assert response.status_code == 401
+    assert response.status_code == 401, (
+        "Auth middleware should working"
+    )
